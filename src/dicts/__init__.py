7f1206f90b5e69a35d403f0e789435a3e94cf7cb
tiagodepalves <tiagodepalves@gmail.com>
2019-11-03 01:27:09 -0300

    Controle do módulo

---
+++ src/dicts/__init__.py
@@ -0,0 +1,5 @@
+from .pairdict import PairDict, DefaultPairDict
+from .tuple_view import TupleView
+from .default_key import DefaultWithKeyDict
+
+__all__ = ["PairDict", "DefaultPairDict", "DefaultWithKeyDict", "TupleView"]
---
+++ src/dicts/pairdict/__init__.py
@@ -0,0 +1,5 @@
+from .pair_dict import PairDict
+from .pair_dict_key import PairDictKey
+from .default_pair_dict import DefaultPairDict
+
+__all__ = ["PairDict", "DefaultPairDict", "PairDictKey"]
